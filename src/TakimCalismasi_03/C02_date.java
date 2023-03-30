48b3440c156ee00da44da98bb73e387f8613d94a
zsk <zsk199207@gmail.com>
2023-03-30 22:49:06 +0300

    takim calismasi_03 2.class

---
+++ src/TakimCalismasi_03/C02_date.java
@@ -0,0 +1,20 @@
+package TakimCalismasi_03;
+
+import java.time.LocalDate;
+
+public class C02_date {
+    public static void main(String[] args) {
+        /*
+            Veli, Ali'den 3 yıl 11 gün sonra doğmuştur.
+        Ali size doğum tarihi bilgisini 24 Kasım 2012 olarak vermiştir.
+        Veli'nin doğum tarihini hesaplamak için gerekli kodu yazınız.
+         */
+
+        LocalDate ali = LocalDate.of(2012, 11, 24);
+
+        LocalDate veli = ali.plusYears(3).plusDays(11);
+        System.out.println(veli);
+
+
+    }
+}
